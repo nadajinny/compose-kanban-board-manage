50fff142a1acf0c3f560a07467a596f050681088
nadajinny <jinsun23007@gmail.com>
2026-03-29 23:57:12 +0900

    refactor: 프로필 아이콘 기본적으로 설정되도록 수정

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/taskcard/Profile.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/taskcard/Profile.kt
@@ -1,8 +1,10 @@
 package woowacourse.kanban.board.model.taskcard
 
+import kanbanboard.composeapp.generated.resources.Res
+import kanbanboard.composeapp.generated.resources.profile
 import org.jetbrains.compose.resources.DrawableResource
 
 data class Profile(
     val nickname: String,
-    val icon: DrawableResource,
+    val icon: DrawableResource = Res.drawable.profile,
 )
