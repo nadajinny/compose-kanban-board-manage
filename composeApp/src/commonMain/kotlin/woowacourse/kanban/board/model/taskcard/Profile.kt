2b8c53c41c521c6fa7184b940cfbf97d597797a5
nadajinny <jinsun23007@gmail.com>
2026-04-01 15:20:45 +0900

    fix: 담당자가 없을 때에 대한 문구 변경(미지정->없음)

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/taskcard/Profile.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/taskcard/Profile.kt
@@ -5,7 +5,7 @@ data class Profile(
     val icon: String = "DEFAULT",
 ) {
     companion object {
-        val NONE = Profile("미지정", "NONE")
+        val NONE = Profile("없음", "NONE")
     }
 
     val isAssigned: Boolean
