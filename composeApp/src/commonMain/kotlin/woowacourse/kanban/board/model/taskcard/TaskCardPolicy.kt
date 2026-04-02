8317c0033e6a100e4495aa301b5f6dc8b3417f1d
nadajinny <jinsun23007@gmail.com>
2026-04-02 10:37:52 +0900

    feat: Review, Done 상태에서는 태스크 삭제가 불가능하다.

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/taskcard/TaskCardPolicy.kt
@@ -0,0 +1,12 @@
+package woowacourse.kanban.board.model.taskcard
+
+object TaskCardPolicy {
+    fun canDelete(status: Status) : Boolean {
+        return when(status) {
+            Status.REVIEW, Status.DONE -> false
+            else -> true
+        }
+    }
+
+
+}
